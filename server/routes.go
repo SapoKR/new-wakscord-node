e9f038f226a6228819956d583847164c3394ba1f
Giyoung Ryu <opentypef@gmail.com>
2023-04-02 14:50:44 +0900

    fix(routes): rename structures

--- server/routes.go
+++ server/routes.go
@@ -4,30 +4,22 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/wakscord/new-wakscord-node/env"
 	"golang.org/x/exp/maps"
 )
 
 func index(ctx *fiber.Ctx) error {
-	return ctx.JSON(NodeStatus{
-		Info: NodeInfo{
-			NodeID: env.GetInt("ID", 1),
-			Owner:  env.GetString("Owner", "Unknown"),
-		},
-		Pending:   NodePending{},
-		Processed: processed,
-		Deleted:   len(deletedWebhooks),
-		Uptime:    int(time.Since(startTime).Seconds()),
-	})
+	status.Deleted = len(deletedWebhooks)
+	status.Uptime = int(time.Since(startTime).Seconds())
+	return ctx.JSON(status)
 }
 
 func request(ctx *fiber.Ctx) error {
-	payload := new(RequestPayload)
+	payload := new(requestPayload)
 	if err := ctx.BodyParser(payload); err != nil {
 		return err
 	}
 
-	go createTasks(payload.Keys, payload.Data)
+	addTask(payload.Keys, payload.Data)
 
 	return ctx.JSON(fiber.Map{"status": "ok"})
 }
