d0c70d7752780cec72495b9bc22388cb04fe5d4b
Giyoung Ryu <opentypef@gmail.com>
2023-04-08 21:49:04 +0900

    feat(discord): use fasthttp

--- discord/request.go
+++ discord/request.go
@@ -1,80 +1,88 @@
 package discord
 
 import (
-	"bytes"
+	// "bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
+
+	// "io"
+	// "net/http"
 	"time"
 
-	// "github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp"
 	"github.com/valyala/fastjson"
 )
 
-// func RequestFastHTTP(key string, data any, retry int) int {
-// 	req := fasthttp.AcquireRequest()
+func RequestFastHTTP(key string, data any, retry int) Response {
+	req := fasthttp.AcquireRequest()
 
-// 	req.SetRequestURI(fmt.Sprintf("%s%s", baseURL, key))
+	req.SetRequestURI(fmt.Sprintf("%s%s", baseURL, key))
 
-// 	req.Header.SetMethod(fasthttp.MethodPost)
-// 	req.Header.SetContentType("application/json")
+	req.Header.SetMethod(fasthttp.MethodPost)
+	req.Header.SetContentType("application/json")
 
-// 	body, _ := json.Marshal(data)
-// 	req.SetBody(body)
-
-// 	resp := fasthttp.AcquireResponse()
-
-// 	err := fasthttpClient.Do(req, resp)
-// 	code := resp.StatusCode()
-// 	if code != fasthttp.StatusNoContent {
-// 		if code == fasthttp.StatusTooManyRequests {
-// 			retryAfter := fastjson.GetFloat64(resp.Body(), "retry_after")
-// 			fmt.Printf("Webhook (%s) is being rate limited. Retrying in %.2f seconds.\n", key[:35], retryAfter)
-// 			time.Sleep(time.Duration(float64(time.Second) * retryAfter))
-// 			if retry != 0 {
-// 				return RequestFastHTTP(key, data, retry-1)
-// 			}
-// 		} else {
-// 			fmt.Printf("Uncaught error occurred. Status Code %d, Error: %s and Body: %s\n", code, err.Error(), resp.Body())
-// 		}
-// 	}
-
-// 	fasthttp.ReleaseRequest(req)
-// 	defer fasthttp.ReleaseResponse(resp)
-
-// 	return resp.StatusCode()
-// }
-
-func RequestHTTP(key string, data any, retry int) int {
 	body, _ := json.Marshal(data)
-	bodyReader := bytes.NewReader(body)
+	req.SetBody(body)
 
-	req, _ := http.NewRequest(http.MethodPost, baseURL+key, bodyReader)
+	resp := fasthttp.AcquireResponse()
 
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
+	err := fasthttpClient.Do(req, resp)
 	if err != nil {
-		fmt.Printf("Uncaught error occurred. Error: %s", err.Error())
-		return 0
-	}
-	defer resp.Body.Close()
-
-	code := resp.StatusCode
-	respBody, _ := io.ReadAll(resp.Body)
-	if code >= 400 && code != http.StatusNotFound {
-		if code == http.StatusTooManyRequests {
-			retryAfter := fastjson.GetFloat64(respBody, "retry_after")
-			fmt.Printf("Webhook (%s) is being rate limited. Retrying in %.2f seconds.\n", key[:35], retryAfter)
-			time.Sleep(time.Duration(float64(time.Second) * retryAfter))
-			if retry != 0 {
-				return RequestHTTP(key, data, retry-1)
-			}
-		} else {
-			fmt.Printf("Uncaught error occurred. Status Code: %d and Body: %s\n", code, string(respBody))
+		return Response{
+			Key:   key,
+			Error: err,
 		}
 	}
 
-	return resp.StatusCode
+	code := resp.StatusCode()
+	if code == fasthttp.StatusTooManyRequests {
+		retryAfter := fastjson.GetFloat64(resp.Body(), "retry_after")
+		fmt.Printf("Webhook (%s) is being rate limited. Retrying in %.2f seconds.\n", key[:35], retryAfter)
+		time.Sleep(time.Duration(float64(time.Second) * retryAfter))
+		if retry != 0 {
+			return RequestFastHTTP(key, data, retry-1)
+		}
+	}
+
+	fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(resp)
+
+	return Response{
+		Key:  key,
+		Code: code,
+		Body: string(resp.Body()),
+	}
 }
+
+// func RequestHTTP(key string, data any, retry int) int {
+// 	body, _ := json.Marshal(data)
+// 	bodyReader := bytes.NewReader(body)
+
+// 	req, _ := http.NewRequest(http.MethodPost, baseURL+key, bodyReader)
+
+// 	req.Header.Set("Content-Type", "application/json")
+
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		fmt.Printf("Uncaught error occurred. Error: %s", err.Error())
+// 		return 0
+// 	}
+// 	defer resp.Body.Close()
+
+// 	code := resp.StatusCode
+// 	respBody, _ := io.ReadAll(resp.Body)
+// 	if code >= 400 && code != http.StatusNotFound {
+// 		if code == http.StatusTooManyRequests {
+// 			retryAfter := fastjson.GetFloat64(respBody, "retry_after")
+// 			fmt.Printf("Webhook (%s) is being rate limited. Retrying in %.2f seconds.\n", key[:35], retryAfter)
+// 			time.Sleep(time.Duration(float64(time.Second) * retryAfter))
+// 			if retry != 0 {
+// 				return RequestHTTP(key, data, retry-1)
+// 			}
+// 		} else {
+// 			fmt.Printf("Uncaught error occurred. Status Code: %d and Body: %s\n", code, string(respBody))
+// 		}
+// 	}
+
+// 	return resp.StatusCode
+// }
